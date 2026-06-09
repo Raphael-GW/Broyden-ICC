4384ba2213ccc7106100cb87d6d0787e19866ff9
Raphael-GW <gaias.raphael@ufpr.br>
2026-06-08 22:00:13 -0300

    Tudo certo para fazer os testes

--- broyden.h
+++ broyden.h
@@ -10,7 +10,7 @@ void avaliaF(const double *x, double *f, int n);
 //newton otimizado
 //void newton(double *a, double *b, double *c, double *f, double *s, double *x, double tol, int max_iter, int n, rtime_t tempoJ, rtime_t tempo_resolucao);
 
-void newton(double **j, double *f, double *s, double *x, double tol, int max_iter, int n, rtime_t tempoJ, rtime_t tempo_resolucao);
+void newton(double **j, double *f, double *s, double *x, double tol, int max_iter, int n, rtime_t *tempoJ, rtime_t *tempo_resolucao);
 
 double norma(const double *v, int n);
 
